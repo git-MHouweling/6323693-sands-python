6844a9c3eca5a7293e23eb408234e006806b2011
Mhouweling <Mathijs.houweling@gmail.com>
2025-09-26 12:22:00 +0200

    Added two more functions, unitstep and halfway through unitpulse

--- gen_signal.py
+++ gen_signal.py
@@ -2,15 +2,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 
-def create_sine_signal(A, F, D, S_R):                  #Amplitude, Frequency, Duration, Sample_rate
-	t=np.linspace(0, D, int(S_R))
-	return np.sin(2*np.pi*F*t)
+def create_sine_signal(A, F, S, E, S_R):                    # Amplitude, Frequency, Start function, End function, Sample_rate
+	N = int((E - S) * S_R)                                  # Total number of samples
+	t = np.linspace(S, E, N)                                # Time
+	y= A*np.sin(2*np.pi*F*t)
+	return t,y
 
-def create_square_signal(A, F, D, S_R):     #Amplitude,Frequency, duration, sample_rate
-    t=np.linspace(0, D, int(S_R))
-    return A*signal.square( np.pi*F* t)
+def create_square_signal(A, F,S, E, S_R):                   # Amplitude,Frequency,Start function, End function, sample_rate
+	N = int((E - S) * S_R)                                  # Total number of samples
+	t = np.linspace(S, E, N)                                # Time
+	y = A*signal.square( np.pi*F* t)
+	return t,y
 
-def create_sawtooth_signal(A,F, D, S_R):     #Amplitude,Frequency, duration, sample_rate
-    t=np.linspace(0, D, int(S_R))
-    return A*signal.sawtooth( np.pi*F* t)
+def create_sawtooth_signal(A,F, S, E, S_R):                 # Amplitude,Frequency, Start function, End function, sample_rate
+	N = int((E - S) * S_R)                                  # Total number of samples
+	t = np.linspace(S, E, N)                                # Time
+	y=A*signal.sawtooth( np.pi*F* t)
+	return t,y
 
+def create_unitstep_signal(A,T,S,E,S_R):                    # Amplitude,Time of step, Start function, End function, sample_rate
+	N = int((E - S) * S_R)                                  # Total number of samples
+	t = np.linspace(S, E, N)                                # Time
+	y = A*(t>=T)
+	return t,y
+
+def create_unitpulse_signal(A,T,L, S,E,S_R):                # Amplitude,Time of step, Start function, End function, sample_rate
+    N = int((E - S) * S_R))                                 # Total number of samples
+	t = np.linspace(S, E, N)                               # Time
+    y= A*()
